7468511b1121cf63e460dbf8f77263cc12f2f981
Topher Bullock <cbullock@pivotal.io>
2017-04-27 14:25:26 -0700

    Revert "add symlinkFinder to return symlinked directories at path"
    This reverts commit b5bdded461c2c184bf6ed473375b29fecc68be85.
    
    Signed-off-by: Topher Bullock <cbullock@pivotal.io>

--- commands/groundcrew.go
+++ commands/groundcrew.go
@@ -1,6 +1,5 @@
 package commands
 
 type GroundcrewCommand struct {
-	Drain                  DrainCommand                  `command:"drain"`
-	AddCertificateSymlinks AddCertificateSymlinksCommand `command:"add-certificate-symlinks"`
+	Drain DrainCommand `command:"drain"`
 }
